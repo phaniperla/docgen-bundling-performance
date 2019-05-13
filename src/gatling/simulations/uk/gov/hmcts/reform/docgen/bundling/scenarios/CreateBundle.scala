fedf0c1d098cd6872077bf05b52982290621a6f9
Jeroen Rijks <jeroen-rijks@hotmail.com>
2019-05-13 12:40:02 +0100

    setup stitch bundle performance test

--- src/gatling/simulations/uk/gov/hmcts/reform/docgen/bundling/scenarios/CreateBundle.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/docgen/bundling/scenarios/CreateBundle.scala
@@ -9,15 +9,15 @@ import scala.io.Source
 object CreateBundle {
 
   val testUtil = new TestUtil();
-  val source: String = Source.fromFile("src/resources/case.json").getLines.mkString
+  val source: String = Source.fromFile("src/resources/create_bundle.json").getLines.mkString
   val json: JsValue = Json.parse(source)
 
   val postCreateBundleReq = http("New Bundle")
     .post("/api/new-bundle")
     .header("Authorization", testUtil.getIdamAuth())
     .header("ServiceAuthorization", testUtil.getS2sAuth())
-    .body(StringBody(json.toString()))
     .header("Content-Type", "application/json")
+    .body(StringBody(json.toString()))
     .check(status is 200)
     .check(bodyString.saveAs("responseBody"))
 
