395d87c5797750f94ab4952b4d07f7d83dd854ed
Jeroen Rijks <jeroen-rijks@hotmail.com>
2019-05-14 17:15:32 +0100

    prep for when we have an sprod dm store doc uri

--- src/gatling/simulations/uk/gov/hmcts/reform/docgen/bundling/scenarios/StitchBundle.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/docgen/bundling/scenarios/StitchBundle.scala
@@ -9,27 +9,22 @@ import scala.io.Source
 object StitchBundle {
 
   val testUtil = new TestUtil();
-  val dmHelper = new DMHelper();
-
   val source: String = Source.fromFile("src/resources/stitch_bundle.json").getLines.mkString
-  val sourceWithDocUri: String = source.format(dmHelper.uploadDocument("src/resources/annotationTemplate.pdf"))
-  //val dmDocument: String = Source.fromFile("src/resources/annotationTemplate.pdf").getLines.mkString
-  val json: JsValue = Json.parse(sourceWithDocUri)
-  // val documentUrl :String = dmHelper.uploadDocument("src/resources/annotationTemplate.pdf")
+  val json: JsValue = Json.parse(source)
 
 
-  val postStitchBundle = http("Stitch Bundle")
+  val postStitchBundleReq = http("Stitch Bundle")
     .post("/api/stitch-ccd-bundles")
     .header("Authorization", testUtil.getIdamAuth())
     .header("ServiceAuthorization", testUtil.getS2sAuth())
     .header("Content-Type", "application/json")
     .body(StringBody(json.toString()))
     .check(status is 200)
-
+    .check(bodyString.saveAs("responseBody"))
 
   val postUser = scenario("Stitch Bundle")
     //.exec(dmHelper.uploadDocument("src/resources/annotationTemplate.pdf"))
-    .exec(postStitchBundle)
+    .exec(postStitchBundleReq)
     .exec { session => println(session("responseBody").as[String]); session}
 
 }
