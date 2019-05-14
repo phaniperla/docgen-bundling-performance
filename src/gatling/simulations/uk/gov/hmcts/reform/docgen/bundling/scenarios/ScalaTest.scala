3948604353ba7919aae78ba57c5510ec82fcd965
PhaniP <phani.perla@hmcts.net>
2019-05-14 11:44:24 +0100

    scala Test code

---
+++ src/gatling/simulations/uk/gov/hmcts/reform/docgen/bundling/scenarios/ScalaTest.scala
@@ -0,0 +1,19 @@
+package simulations.uk.gov.hmcts.reform.docgen.bundling.scenarios
+
+import play.api.libs.json.{JsValue, Json}
+
+import scala.io.Source
+
+object ScalaTest {
+
+  object Main {
+    def main(args: Array[String]) {
+      println( "Returned Value : " );
+    }
+
+    val source: String = Source.fromFile("src/resources/case.json").getLines.mkString
+    val json: JsValue = Json.parse(source)
+    print(json)
+  }
+
+}
